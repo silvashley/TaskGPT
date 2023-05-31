64f05a3cbbd8eea77846da373e6af1a448f59ee2
Ashley Silva <silvashleya@gmail.com>
2023-05-31 13:23:21 -0400

    create file

---
+++ app.js
@@ -0,0 +1,17 @@
+//c style
+
+//1. define the problem
+//1a when a user clicks a div
+//
+
+
+
+const myBtn="stuff";
+const myName="dele";
+const myPlace="ulem";
+
+//var called "mybtn" that has stuff
+
+console.log(myBtn)
+
+//1b. i see a pop-up
